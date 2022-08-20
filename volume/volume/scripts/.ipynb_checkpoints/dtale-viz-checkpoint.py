a1907363df794cfe85ae97e8ccabb506b8eddf42
DhoungSALENE____ <61023531+salene96x@users.noreply.github.com>
2022-08-20 16:24:55 +0700

    update: able to eda with Dtale
    - tracking added

---
+++ volume/volume/scripts/.ipynb_checkpoints/dtale-viz-checkpoint.py
@@ -0,0 +1,20 @@
+from flask import redirect
+import pandas as pd
+import dtale
+from dtale.app import build_app
+from dtale.views import startup
+import sys
+if __name__ == '__main__':
+    app = build_app(reaper_on=False)
+    file = f'/usr/src/volume/volume/datasets/{sys.argv[1]}'
+    @app.route("/create-df")
+    def create_df():
+        df = pd.read_csv(file)
+        instance = startup(data=df, ignore_duplicate=True)
+        return redirect(f"/dtale/main/{instance._data_id}", code=302)
+
+    @app.route("/")
+    def hello_world():
+        return 'Hi there, load data using <a href="/create-df">create-df</a>'
+
+    app.run(host="0.0.0.0", port=8181)
